0e084e16503280c2e76f7ff5e38e127def911c74
Vladyslav <lefttwixwand@gmail.com>
2022-01-01 22:52:41 +0200

    feat: Add db context

--- CommanderGQL/Program.cs
+++ CommanderGQL/Program.cs
@@ -1,4 +1,15 @@
+using CommanderGQL.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+                                        options.UseSqlServer(
+                                            builder.Configuration.GetConnectionString("CommandConnectionString")));
+
+
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
